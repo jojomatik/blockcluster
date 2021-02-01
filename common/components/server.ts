efce3354b51322d753d342cbf10234d71808d929
jojomatik <dev@jojomatik.de>
2021-02-01 21:32:03 +0100

    Added typescript documentation.

--- common/components/server.ts
+++ common/components/server.ts
@@ -1,42 +1,87 @@
+/**
+ * An enum that holds different states a {@link Server} can be in.
+ */
 export enum ServerStatus {
   Offline,
   Online,
   Unknown
 }
 
+/**
+ * A minecraft server.
+ */
 export default class Server {
+  /**
+   * Creates an instance of {@link Server} based on its name, its current state and its port.
+   * @param name the name of the server.
+   * @param status the status of the server.
+   * @param port the port the server listens on.
+   */
   constructor(name: string, status: ServerStatus, port: number) {
     this._name = name;
     this._status = status;
     this._port = port;
   }
 
+  /**
+   * The name of the {@link Server}.
+   * @private
+   */
   private _name: string;
 
+  /**
+   * Returns {@link #name}.
+   */
   get name(): string {
     return this._name;
   }
 
+  /**
+   * Sets a new value for {@link #name}.
+   * @param value the new value.
+   */
   set name(value: string) {
     this._name = value;
   }
 
+  /**
+   * The current {@link ServerStatus} of the {@link Server}.
+   * @private
+   */
   private _status: ServerStatus = ServerStatus.Unknown;
 
+  /**
+   * Returns {@link #status}.
+   */
   get status(): ServerStatus {
     return this._status;
   }
 
+  /**
+   * Sets a new value for {@link #status}.
+   * @param value the new value.
+   */
   set status(value: ServerStatus) {
     this._status = value;
   }
 
+  /**
+   * The port that the {@link Server} listens to.
+   * @private
+   */
   private _port: number;
 
+  /**
+   * Returns {@link #status}.
+   */
   get port(): number {
     return this._port;
   }
 
+  /**
+   * The port that the {@link Server} listens to.
+   * @private
+   */
   set port(value: number) {
     this._port = value;
   }
--- src/components/ServerComponent.vue
+++ src/components/ServerComponent.vue
@@ -11,10 +11,22 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 
 import Server, { ServerStatus } from "../../common/components/server";
 
+/**
+ * The representation of a {@link Server} in Vue.
+ */
 @Component
 export default class ServerComponent extends Vue {
+  /**
+   * The linked {@link Server}.
+   * @private
+   */
   @Prop() private server!: Server;
 
+  /**
+   * Returns the enum constant name for the {@link server}s status.
+   * @return the enum constant name for the {@link server}s status.
+   * @private
+   */
   private getStatus(): string {
     return ServerStatus[this.server.status];
   }

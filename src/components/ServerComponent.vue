cd48ff30ba486f5d923e5d254fb99e7e286d8d2d
jojomatik <dev@jojomatik.de>
2021-03-18 20:21:33 +0100

    Use table design in undetailed mode.

--- src/components/ServerComponent.vue
+++ src/components/ServerComponent.vue
@@ -94,7 +94,21 @@
       <ServerStatusComponent :status="server.status" />
     </v-card-title>
     <v-card-text>
-      <div>Port: {{ server.port }}</div>
+      <v-row>
+        <v-col cols="6">
+          <v-simple-table>
+            <colgroup>
+              <col style="width: calc(100% / 4)" />
+            </colgroup>
+            <tbody>
+              <tr>
+                <td>Port</td>
+                <td>{{ server.port }}</td>
+              </tr>
+            </tbody>
+          </v-simple-table>
+        </v-col>
+      </v-row>
     </v-card-text>
     <v-card-actions>
       <v-btn :to="'/server/' + encodeURIComponent(server.name)">

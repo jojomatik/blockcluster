67ff68f1b3b76f5288cdb4204c9ddd32c68c3bf2
jojomatik <dev@jojomatik.de>
2021-07-03 20:26:39 +0200

    ci: release using semantic release
    If a new feature, a bug fix or a BREAKING CHANGE is pushed to master, the package versions are bumped and a new release is created automatically.

---
+++ release.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+        releaseRules: [
+          {
+            breaking: true,
+            release:
+              process.env.npm_package_version.split(".")[0] > 0
+                ? "major"
+                : "minor",
+          },
+        ],
+      },
+    ],
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd:
+          "npm version ${nextRelease.version} --allow-same-version --no-git-tag-version && cd backend && npm version ${nextRelease.version} --allow-same-version --no-git-tag-version",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["**/package*.json"],
+        message:
+          "release: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};

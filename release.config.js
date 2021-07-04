7561dd5c94af75f0ecb72e38fe7e129190aac00d
jojomatik <dev@jojomatik.de>
2021-07-04 22:15:50 +0200

    ci: remove release notes from release commit
    Release notes should not be included in the release commit as they are not connected to the changes of the commit itself.

--- release.config.js
+++ release.config.js
@@ -28,8 +28,7 @@ module.exports = {
       "@semantic-release/git",
       {
         assets: [["**/package*.json", "!node_modules/**"]],
-        message:
-          "release: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+        message: "release: ${nextRelease.version} [skip ci]",
       },
     ],
   ],

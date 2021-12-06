jojomatik/blockcluster

commit 4c1edd6dec214f9ea15210b6613b9fb23354e6a0

refactor: remove redundant `await`s
Removes three redundant `await`s for non-promise typed functions.

src/components/ConsoleComponent.vue
@@ -150,7 +150,7 @@ export default class ConsoleComponent extends Vue {
                   data["message"][i],
                   data["message"][i - 1]._timestamp
                 );
-              await this.messages.push(
+              this.messages.push(
                 Object.assign(new Message(), data["message"][i])
               );
             }
@@ -168,9 +168,7 @@ export default class ConsoleComponent extends Vue {
                 },
                 this.messages[len - 1].timestamp
               );
-            await this.messages.push(
-              Object.assign(new Message(), data["message"])
-            );
+            this.messages.push(Object.assign(new Message(), data["message"]));
           }
           this.scrollConsole();
         }
@@ -204,7 +202,7 @@ export default class ConsoleComponent extends Vue {
       Math.floor(lastTimestamp / (24 * 60 * 60 * 1000)) <
         Math.floor(message._timestamp / (24 * 60 * 60 * 1000))
     )
-      await this.messages.push(
+      this.messages.push(
         new Message(MessageType.DateChange, newDate.format("YYYY-MM-DD"))
       );
   }

commit f505603ebefbc80471cb54c23b970825698f6d7a

fix: scroll console to last message
Scrolls console to last message again by waiting one tick between adding the messages and scrolling down. The problem was probably introduced in 4c1edd6dec214f9ea15210b6613b9fb23354e6a0 but the new solution is more elegant than a revert, as the previous `await`s should in theory have no effect on the order of operations.

Fixes #243

src/components/ConsoleComponent.vue
@@ -170,7 +170,7 @@ export default class ConsoleComponent extends Vue {
               );
             this.messages.push(Object.assign(new Message(), data["message"]));
           }
-          this.scrollConsole();
+          Vue.nextTick(this.scrollConsole);
         }
       }
     );
1b57ceb032c699c2fdf1d78136a4be3b2ba5a497
jojomatik <dev@jojomatik.de>
2022-02-19 01:08:37 +0100

    ci: add `main` as release branch
    Add branch `main` as release branch instead of `master` in semantic-release configuration.

--- release.config.js
+++ release.config.js
@@ -69,4 +69,12 @@ module.exports = {
       },
     ],
   ],
+  branches: [
+    "+([0-9])?(.{+([0-9]),x}).x",
+    "main",
+    "next",
+    "next-major",
+    { name: "beta", prerelease: true },
+    { name: "alpha", prerelease: true },
+  ],
 };
